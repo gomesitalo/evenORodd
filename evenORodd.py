41613f3adc936e1a25ff3d12f10f1c067d6ad80d
gomesitalo <italo.souza2100@gmail.com>
2024-11-03 18:10:11 -0300

    first commit

---
+++ evenORodd.py
@@ -0,0 +1,5 @@
+# Uma função simples para determinar se um número inteiro é 'ímpar' ou 'par'.  =}
+
+while 'True':
+    num = int(input("Digite um número inteiro: "))
+    print("PÍamrp a r"[num%2::2]) # função 'Ímpar ou Par'
